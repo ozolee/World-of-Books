ee8c34b697aadfbd5cca6561d37929f6451831b1
ozolee <34374771+ozolee@users.noreply.github.com>
2017-12-08 21:07:20 +0100

    -

--- application/config/development/config.php
+++ application/config/development/config.php
@@ -15,7 +15,7 @@
 |
 */
 
-$config['base_url']	= 'http://localhost/WOB';
+$config['base_url']	= 'http://localhost/World-of-Buzz';
 
 /*
 |--------------------------------------------------------------------------
